cc0fb9cf32d72d46a6a262db1ae72a8e9166eb48
The-Canuck <morrisonman56@gmail.com>
2018-05-16 20:58:55 -0600

    Add searchview
    - Add searchview to actionbar, currently only creates a toast

--- app/src/main/java/com/the_canuck/openpodcast/activities/MainActivity.java
+++ app/src/main/java/com/the_canuck/openpodcast/activities/MainActivity.java
@@ -1,5 +1,8 @@
 package com.the_canuck.openpodcast.activities;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -7,12 +10,17 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.the_canuck.openpodcast.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private DrawerLayout mDrawerLayout;
 
@@ -20,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handleIntent(getIntent());
 
         mDrawerLayout = findViewById(R.id.drawer_layout);
         
@@ -51,7 +61,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    /**
+     * Takes the search query from the searchview intent and sends it to the search fragment.
+     *
+     * @param intent from the searchview, which holds query that contains user input
+     */
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.v(LOG_TAG, "handleIntent(): query = " + query);
+            //TODO: Do stuff with the query info, likely send to search fragment in bundle
+            Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()) {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
@@ -59,4 +89,23 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setSubmitButtonEnabled(true);
+
+        // This code below can change the icons of the search bar
+//        ImageView searchCloseIcon = searchView.findViewById(android.support.v7.appcompat.R.id.search_close_btn);
+//        searchCloseIcon.setImageResource(R.drawable.ic_close_white_24dp);
+
+        return true;
+    }
+
+
 }
