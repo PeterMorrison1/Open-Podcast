d24385b77f0465a06fdcae54061c15cb432255b4
The-Canuck <morrisonman56@gmail.com>
2018-06-09 15:42:31 -0600

    Clean and comment RssReader update gradle
    - Clean rssreader of redundent code
    - Remove android XML call for a XmlPullParserFactory
    - Add docs and comments to RssReader
    - Update gradle to newest version

--- app/src/main/java/com/the_canuck/openpodcast/search/RssReader.java
+++ app/src/main/java/com/the_canuck/openpodcast/search/RssReader.java
@@ -1,11 +1,10 @@
 package com.the_canuck.openpodcast.search;
 
-import android.util.Xml;
-
 import com.the_canuck.openpodcast.Episode;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,9 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RssReader {
-//    private String testUrl = "https://adventurezone.libsyn.com/rss";
-    private String testUrl;
-    private List<Episode> episodeList = new ArrayList<>();
+    private String url;
     private String podcastDescription;
     static final String TITLE = "title";
     static final String DESCRIPTION = "description";
@@ -25,22 +22,24 @@ public class RssReader {
     static final String PUB_DATE = "pubDate";
     static final String CHANNEL = "channel";
 
-    public RssReader(String testUrl) {
-        this.testUrl = testUrl;
+    public RssReader(String url) {
+        this.url = url;
     }
 
-
-    public RssReader() {
-    }
-
+    /**
+     * Parses RSS link and puts items (episodes) into an array.
+     *
+     * @return array of parsed episodes
+     */
     public List<Episode> createEpisodeList() {
         List<Episode> list = new ArrayList<>();
-        XmlPullParser xmlParser = Xml.newPullParser();
         InputStream stream = null;
         String text = null;
 
         try {
-            stream = new URL(testUrl).openConnection().getInputStream();
+            XmlPullParser xmlParser = XmlPullParserFactory.newInstance().newPullParser();
+
+            stream = new URL(url).openConnection().getInputStream();
             xmlParser.setInput(stream, null);
 
             int event = xmlParser.getEventType();
@@ -48,7 +47,7 @@ public class RssReader {
             Episode item = null;
 
             while (event != XmlPullParser.END_DOCUMENT && !done) {
-                String name = null;
+                String name;
                 String descriptionParser = xmlParser.getName();
 
                 switch (event) {
@@ -57,6 +56,8 @@ public class RssReader {
 
                     case XmlPullParser.START_TAG:
                         name = xmlParser.getName();
+
+                        // parses anything within an "item" tag (so each episode listed)
                         if (name.equalsIgnoreCase(ITEM)) {
                             item = new Episode();
                         } else if (item != null) {
@@ -78,10 +79,12 @@ public class RssReader {
                         break;
 
                     case XmlPullParser.END_TAG:
+                        // finds description tag in html for the whole rss feed (not per episode)
                         if (descriptionParser.equalsIgnoreCase(DESCRIPTION)) {
                             podcastDescription = text;
                         }
 
+                        // check for end of item or end of RSS feed
                         name = xmlParser.getName();
                         if (name.equalsIgnoreCase(ITEM) && item != null) {
                             list.add(item);
@@ -107,6 +110,11 @@ public class RssReader {
         return list;
     }
 
+    /**
+     * Gets parsed description for whole podcast (not individual episodes).
+     *
+     * @return parsed string of description
+     */
     public String getPodcastDescription() {
         return podcastDescription;
     }
